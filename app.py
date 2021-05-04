aece14ff30d9110c911ab29f7d6ff636e1f216c7
Hien Pham <phaminhien@hotmail.com>
2021-05-04 11:01:48 +0700

    read posts

--- app.py
+++ app.py
@@ -1,11 +1,41 @@
 from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
+app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+Bootstrap(app)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///posts.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+
+class BlogPost(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(250), unique=True, nullable=False)
+    subtitle = db.Column(db.String(250), nullable=False)
+    date = db.Column(db.String(250), nullable=False)
+    body = db.Column(db.Text, nullable=False)
+    author = db.Column(db.String(250), nullable=False)
+    img_url = db.Column(db.String(250), nullable=False)
+
+
+db.create_all()
+
 
 @app.route('/')
-def index():
-    return render_template('index.html')
+def home():
+    posts = BlogPost.query.all()
+    return render_template('index.html', posts=posts)
+
+
+@app.route('/post/<int:post_id>')
+def post(post_id):
+    requested_post = BlogPost.query.get(post_id)
+    print(requested_post.body)
+    return render_template('post.html', post=requested_post)
 
 
 @app.route('/about')
